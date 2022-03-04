34a18bfc71a56384bf897f89f65100fc79d5df02
TarleyRodrigues <tarley.divino@gmail.com>
2022-03-04 15:45:30 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,20 +34,17 @@ public class Program {
 			System.out.print("Check-Out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: reservation dates for update must be future date!");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date!");
+			
 
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-
-			reservation.updateDates(checkIn, checkOut);
 			System.out.println(reservation);
-		}}
-
+		
+			}
+		}
 		sc.close();
 
 	}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 public class Reservation {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
@@ -43,9 +43,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future date!";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date!";
+		}
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		
+		return null;
 	}
 
 	@Override
