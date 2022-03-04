b3bc8e0f69cdef5baf453b365dcaacf9e57ecc47
TarleyRodrigues <tarley.divino@gmail.com>
2022-03-04 17:04:56 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,45 +6,52 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Room number: ");
-		int number = sc.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-Out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date!");
-		} else {
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-Out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+	
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println(reservation);
-
+	
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-Out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-
-			
-
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Error in reservation: " + error);
-			}
-			else {
+	
+			reservation.updateDates(checkIn, checkOut);
 			System.out.println(reservation);
-		
-			}
 		}
+		//capturando o tipo de exceção Parse
+		catch(ParseException e){
+			System.out.println("Invalid date format!");
+		}
+		//capturando o tipo de exceção personalizada Domain
+		catch(DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		//Catch com um tipo de exceção genérico, para fazer o upcasting para RuntimeException e dar a mensagem genérica de erro:
+		//Para o programa não quebrar, utilizar esse cacth para capturar qualquer outra exceção inesperada:
+		//InputMismatchException (quando o valor de entrada é errado) também é uma RuntimeException.
+		catch(RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		
 		sc.close();
 
 	}
